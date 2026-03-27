3a4b32df9090b0d36cf0236d242571a3e6375b8f
Armaan Mathur <amathur2500@gmail.com>
2026-03-26 21:00:15 -0400

    Add synchronizers and edge detection

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -21,4 +21,35 @@ always @(posedge clk or negedge rst_n) begin
     end
 end
 
+
+// Synchronizers
+reg sclk_sync1, sclk_sync2;
+reg copi_sync1, copi_sync2;
+reg ncs_sync1, ncs_sync2;
+
+always @(posedge clk) begin
+    sclk_sync1 <= sclk;
+    sclk_sync2 <= sclk_sync1;
+
+    copi_sync1 <= copi;
+    copi_sync2 <= copi_sync1;
+
+    ncs_sync1 <= ncs;
+    ncs_sync2 <= ncs_sync1;
+end
+
+// Edge detection
+reg sclk_prev;
+reg ncs_prev;
+
+always @(posedge clk) begin
+    sclk_prev <= sclk_sync2;
+    ncs_prev  <= ncs_sync2;
+end
+
+wire sclk_rising = (sclk_sync2 == 1'b1) && (sclk_prev == 1'b0);
+wire ncs_rising  = (ncs_sync2  == 1'b1) && (ncs_prev  == 1'b0);
+wire ncs_falling = (ncs_sync2  == 1'b0) && (ncs_prev  == 1'b1);
+
+
 endmodule
